9cc72a7ffc4bbbdc8e2e29196e3b406735a0fd04
mumblecoder <mumblecoder@gmail.com>
2021-02-11 21:38:46 +0900

    jpa 연동, entity / repository 생성 및 테스트

---
+++ src/main/java/com/example/boot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.example.boot.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(length = 500, nullable = false)
+    String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    String content;
+
+    String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
---
+++ src/test/java/com/example/boot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.example.boot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        String title = "테스트 게시글";
+        String content = "TEST";
+
+        postsRepository.save(Posts.builder().title(title)
+                                            .content(content)
+                                            .author("mumblecoder")
+                                            .build()
+        );
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}

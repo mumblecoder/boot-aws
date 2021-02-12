99d02132c5c5f6bf1e3c970e45503c858f7f7702
mumblecoder <mumblecoder@gmail.com>
2021-02-12 20:39:03 +0900

    생성, 조회, 수정 기능 API 구현 및 테스트

---
+++ src/main/java/com/example/boot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.boot.web.dto;
+
+import com.example.boot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}

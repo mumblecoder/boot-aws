b9c74681d10156414d5cc6654518803b4ac548c4
mumblecoder <mumblecoder@gmail.com>
2021-02-16 23:03:02 +0900

    session에서 유저 정보 가져오는 방식을 어노테이션 사용하여 처리하도록 변경

--- src/main/java/com/example/boot/web/IndexController.java
+++ src/main/java/com/example/boot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.boot.web;
 
+import com.example.boot.config.auth.LoginUser;
 import com.example.boot.config.auth.dto.SessionUser;
 import com.example.boot.web.dto.PostsResponseDto;
 import com.example.boot.web.service.posts.PostsService;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());

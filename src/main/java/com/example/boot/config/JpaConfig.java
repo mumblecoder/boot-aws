2347a5dd2d141a522dcfe1ce8dd4fc3c0bb530fd
mumblecoder <mumblecoder@gmail.com>
2021-02-19 21:30:22 +0900

    누락분 커밋

---
+++ src/main/java/com/example/boot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
